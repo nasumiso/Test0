a625f6c8f9db62b27e0b539efd693078914a22d3
K.Sohara <srqys795@yahoo.co.jp>
2025-03-29 12:17:25 +0900

    テストプロジェクト登録

---
+++ testproject/testproject/Test.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace SampleConsole
+{
+    class Program
+    {
+        static void Swap(ref int l, ref int r)
+        {
+            var tmp = l;
+            l = r;
+            r = tmp;
+        }
+
+        static void Main(string[] args)
+        {
+            int l = 1, r = -1;
+            Console.WriteLine($"left= {l}, right= {r}"); // <- ここに Break point
+            Swap(ref l, ref r);
+            Console.WriteLine($"left= {l}, right= {r}");
+        }
+    }
+}
